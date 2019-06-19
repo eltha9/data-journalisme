8e33480b9287f181b5078e005ea28c66a487e36a
Eloise Isautier <eloise.isautier@hetic.net>
2019-06-19 19:44:40 +0200

    ugly menu lol

--- Front/front-page/script.js
+++ Front/front-page/script.js
@@ -9,10 +9,12 @@ menuBtn.addEventListener('click', ()=>
     if(menuOpen)
     {
         menu.classList.remove('open')
+        menuBtn.classList.remove('open')
         menuOpen=false
     } else
     {
         menu.classList.add('open')
+        menuBtn.classList.add('open')
         menuOpen=true
     }
 })
