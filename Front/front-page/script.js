068d664ce7f60657bcdf5dcd8240eadbd38d0e1f
Eloise Isautier <eloise.isautier@hetic.net>
2019-06-19 20:25:56 +0200

    search

--- Front/front-page/script.js
+++ Front/front-page/script.js
@@ -2,19 +2,37 @@
 const menu = document.querySelector('.open-menu')
 const menuBtn = document.querySelector('.burger-menu')
 
-let menuOpen=false 
+const search = document.querySelector('.open-search')
+const searchBtn = document.querySelector('.search-menu')
+
+let burgerMenuOpen=false 
+let searchMenuOpen=false 
 
 menuBtn.addEventListener('click', ()=>
 {
-    if(menuOpen)
+    if(burgerMenuOpen)
     {
         menu.classList.remove('open')
         menuBtn.classList.remove('open')
-        menuOpen=false
+        burgerMenuOpen=false
     } else
     {
         menu.classList.add('open')
         menuBtn.classList.add('open')
-        menuOpen=true
+        burgerMenuOpen=true
+    }
+})
+
+
+searchBtn.addEventListener('click', ()=>
+{
+    if(searchMenuOpen)
+    {
+        search.classList.remove('open')
+        searchMenuOpen=false
+    } else
+    {
+        search.classList.add('open')
+        searchMenuOpen=true
     }
 })
