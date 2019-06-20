e63cb4d43cf7db7b4d7c80d00da95d57f931360a
Eloise Isautier <eloise.isautier@hetic.net>
2019-06-20 09:18:21 +0200

    finito presto v1

--- Front/article/script.js
+++ Front/article/script.js
@@ -7,3 +7,42 @@ videoBtn.addEventListener('click', ()=>
     video.play()
     video.requestFullscreen()
 })
+
+
+const menu = document.querySelector('.open-menu')
+const menuBtn = document.querySelector('.burger-menu')
+
+const search = document.querySelector('.open-search')
+const searchBtn = document.querySelector('.search-menu')
+
+let burgerMenuOpen=false 
+let searchMenuOpen=false 
+
+menuBtn.addEventListener('click', ()=>
+{
+    if(burgerMenuOpen)
+    {
+        menu.classList.remove('open')
+        menuBtn.classList.remove('open')
+        burgerMenuOpen=false
+    } else
+    {
+        menu.classList.add('open')
+        menuBtn.classList.add('open')
+        burgerMenuOpen=true
+    }
+})
+
+
+searchBtn.addEventListener('click', ()=>
+{
+    if(searchMenuOpen)
+    {
+        search.classList.remove('open')
+        searchMenuOpen=false
+    } else
+    {
+        search.classList.add('open')
+        searchMenuOpen=true
+    }
+})
